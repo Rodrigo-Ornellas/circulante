e9343c03a69a11cef6189c0f1ce2f48e023251eb
Rodrigo Ornellas <rodrigo.ornellas@mail.com>
2012-04-19 23:07:57 -0300

    criamos o comando import para importar os dados de uma arquivo texto para a base de dados

--- circulante/circulante/catalogo/models.py
+++ circulante/circulante/catalogo/models.py
@@ -2,13 +2,20 @@
 from django.db import models
 
 TIPOS_PUBLICACAO = (
-    (u'livro', u'Livro'),
-    (u'apostila', u'Apostila'),
-    (u'gibi', u'Gibi'),
+    (u'livro', u'livro'),
+    (u'apostila', u'apostila'),
+    (u'gibi', u'gibi'),
+    (u'outro', u'outro'),    
 )
 
 class Publicacao(models.Model):
     tipo = models.CharField(max_length=16, choices=TIPOS_PUBLICACAO, default=TIPOS_PUBLICACAO[0][0])
     id_padrao = models.CharField(max_length=32, blank=True)
     titulo = models.CharField(max_length=256)
-         
+    #autores = models.ManyToManyField('Autor')
+    num_paginas = models.PositiveIntegerField(default=0)
+
+class Credito(models.Model):
+    nome = models.CharField(max_length=256)
+    papel = models.CharField(max_length=32, blank=True)
+    publicacao = models.ForeignKey(Publicacao)
